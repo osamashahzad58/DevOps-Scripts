660ba7f270abab7ee2f27a9cffe7f65c849f08e0
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-10-23 15:53:07 +0500

    Added: Server Setup Scripts

---
+++ Server-Setup-Scripts/development-server-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+sudo apt update
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install node
+
+npm install pm2 -g
+
+sudo apt-get install -y lsb-release curl gpg
+
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+sudo apt-get update
+sudo apt-get install -y redis
+
+sudo systemctl enable redis-server
+sudo systemctl start redis-server
