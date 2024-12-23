387c60a7f375a4f109a316be8d5705f9474821fa
Muhammad Osama <muhammadosamashahzad@outlook.com>
2024-12-23 20:00:24 +0500

    Update development-server-setup.sh

--- Server-Setup-Scripts/development-server-setup.sh
+++ Server-Setup-Scripts/development-server-setup.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-set -e
+set -euxo pipefail
 
+# Update and install prerequisites
 sudo apt update
+sudo apt-get install -y lsb-release curl gpg
 
+# Install NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bashrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.bashrc
 
+# Install Node.js and PM2
 nvm install node
-
 npm install pm2 -g
 
-sudo apt-get install -y lsb-release curl gpg
-
+# Add Redis repository and install Redis
 curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 sudo apt-get update
 sudo apt-get install -y redis
 
+# Enable and start Redis
 sudo systemctl enable redis-server
 sudo systemctl start redis-server
+
+source ~/.bashrc
